7d68ae75c5e8c63ab9232e3ea5a4f23e6b7d0154
Piper <peppypiper14@gmail.com>
2022-08-09 23:55:31 -0500

    init

---
+++ routes.js
@@ -0,0 +1,21 @@
+const { REST, Routes } = require('discord.js');
+const secrets = require('./secrets.json');
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'h',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(secrets.token);
+
+(async () => {
+    try {
+        console.log("Start");
+        await rest.put(Routes.applicationGuildCommands('1006734097831432278', '1006734321383645246'), { body: commands });
+        console.log("Done");
+    } catch (error) {
+        console.error(error);
+    }
+})();

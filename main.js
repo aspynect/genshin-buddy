e58b51b1bfca98424cb86968ab1bd0d0d1ca1ab7
Piper <peppypiper14@gmail.com>
2022-08-11 01:01:33 -0500

    added uid command :3

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const fs = require('fs/promises')
 const secrets = require('./secrets.json');
+const users = require('./data/users.json')
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,8 +11,24 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'hello') {
+    if (interaction.commandName === 'ping') {
         await interaction.reply('h');
+    }
+
+    if (interaction.commandName === 'uid') {
+        const user = interaction.options.getUser('user');
+        const uid = interaction.options.getString('uid')
+        if (user && uid) {
+            await interaction.reply({content: "Failure: Use only one parameter", ephemeral: true})
+        }else if (user) {
+            await interaction.reply({content: `<@${user.id}>'s UID is ${users[user.id]}`, ephemeral: true})
+        } else if (uid) {
+            users[interaction.member.id] = uid
+            await fs.writeFile('./data/users.json', JSON.stringify(users))
+            await interaction.reply({content: `UID set to ${uid}`, ephemeral: true})
+        } else {
+            await interaction.reply({content: "Failure: Parameter required", ephemeral: true})
+        }
     }
 });
 

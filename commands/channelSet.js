f69a357089e1753664415cd6377b5ac9fc1b14be
Piper <peppypiper14@gmail.com>
2022-08-17 00:28:22 -0500

    the last one except yeah

--- commands/channelSet.js
+++ commands/channelSet.js
@@ -4,7 +4,7 @@ const serverList = require('../data/ignored/servers.json')
 async function setChannel(guild, channel, interaction) {
     if (channel){
         serverList[guild.id] = channel.id
-        await fs.writeFile('./data/servers.json', JSON.stringify(serverList))
+        await fs.writeFile('./data/ignored/servers.json', JSON.stringify(serverList))
         if (interaction) {await interaction.reply({content: `Bot channel set to <#${channel.id}>`, ephemeral: true})}
     } else {
         if (interaction) {await interaction.reply({content: `Please input a channel to set as your Bot Channel`, ephemeral: true})}

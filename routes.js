d7b514b20ccc424faae41d373be9bdd87dfc6a6c
PistachioPiper <peppypiper14@gmail.com>
2022-12-02 11:33:16 -0600

    a.

--- routes.js
+++ routes.js
@@ -109,7 +109,7 @@ const rest = new REST({ version: '10' }).setToken(secrets.token);
 (async () => {
     try {
         console.log("Started routing");
-        await rest.put(Routes.applicationGuildCommands(`${secrets.appID}`, '1006734321383645246'), { body: specialCommands });
+        await rest.put(Routes.applicationGuildCommands(`${1046277143274991626}`, '1006734321383645246'), { body: specialCommands });
         await rest.put(Routes.applicationCommands(`${secrets.appID}`), { body: commands });
         console.log("Routing complete");
     } catch (error) {
